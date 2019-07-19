07b5dff6d96973aeb83a53a565314dba4a99ba1d
Sh2351976 <shatha.m.abdo@gmail.com>
2019-07-19 11:50:05 +0200

    docker1
    Signed-off-by: Sh2351976 <shatha.m.abdo@gmail.com>

--- Dockerfile.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM java:8
-VOLUME /tmp
-ADD /target/docker-spring-mongo.jar docker-spring-mongo.jar
-EXPOSE 5000
-RUN bash -c 'touch /docker-spring-mongo.jar'
-ENTRYPOINT  ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar","/docker-spring-mongo.jar"]
--- src/main/java/se/BTH/ITProjectManagement/ItProjectManagementApplication.java
+++ src/main/java/se/BTH/ITProjectManagement/ItProjectManagementApplication.java
@@ -1,11 +1,16 @@
 package se.BTH.ITProjectManagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})//add to config docker
 public class ItProjectManagementApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
