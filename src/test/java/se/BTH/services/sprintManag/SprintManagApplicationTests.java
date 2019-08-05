06a652b30037949f3597387c0148a1fb54b8b842
Sh2351976 <shatha.m.abdo@gmail.com>
2019-08-05 10:07:43 +0200

    sprint manag services done

--- src/test/java/se/BTH/services/sprintManag/SprintManagApplicationTests.java
+++ src/test/java/se/BTH/services/sprintManag/SprintManagApplicationTests.java
@@ -1,4 +1,4 @@
-package se.BTH.ITProjectManagement;
+package se.BTH.services.sprintManag;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ItProjectManagementApplicationTests {
+public class SprintManagApplicationTests {
 
 	@Test
 	public void contextLoads() {
